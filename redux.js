a1842eebb356763515c1792e3164707bfc081c0b
TshireletsoMogwere <tmogwere09@gmail.com>
2024-05-22 09:50:40 +0200

    created a store

--- redux.js
+++ redux.js
@@ -10,3 +10,17 @@ const reducer = (state = initialState, action) => {
         default: return state;
     }
 }
+
+// Implements a store
+const createTallyStore = (reducer) => {
+    let state; // holds current state 
+    let listener = []; // holds called functions whenever state changes
+
+    const getState = () => state; // returns current state
+    
+    // Sends actions to the store
+    const dispatch = (action) => {
+        state = (state, action);
+        listeners.forEach(listener => listener())
+    }
+};
