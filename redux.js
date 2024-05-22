94f8cf5de765a5c11cf7f78d8b8c3b35bedc8410
TshireletsoMogwere <tmogwere09@gmail.com>
2024-05-22 09:38:28 +0200

    created update function

--- redux.js
+++ redux.js
@@ -1,4 +1,12 @@
 // Define default state
 const initialState = {count : 0}
 
-// 
+// updateFunction
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'ADD': return { ...state.count + 1};
+        case 'SUBSTRACT' : return {...state.count + 1};
+        case 'RESET' : return {...state.count + 1};
+        default: return state;
+    }
+}
